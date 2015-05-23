dda59e22afbaecd9bdc8a989ea60099512851917
Mihails Volkovs <mihails.volkovs@gmail.com>
2015-05-23 16:15:47 +0300

    My solution via initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions set allowing cache inverted matrix 
+## inside "matrix" object.
 
-## Write a short comment describing this function
-
+## Creates a "matrix" object able to cache inverted 
+## version of itself. 
+## Function parameter should be a square matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+    solved <- NULL
+    set <- function(matrix) {
+        x <<- matrix
+        solved <<- NULL
+    }
+    get <- function() x
+    setSolved <- function(matrix) solved <<- matrix
+    getSolved <- function() solved
+    list(set = set, get = get,
+         setSolved = setSolved,
+         getSolved = getSolved)
 }
 
-
-## Write a short comment describing this function
-
+## Returns inverted version of given "matrix" object.
+## Cached version is returned 
+## (if already calculated previously).
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    solved <- x$getSolved()
+    if(!is.null(solved)) {
+        message("getting cached data")
+        return(solved)
+    }
+    data <- x$get()
+    solved <- solve(data, ...)
+    x$setSolved(solved)
+    solved
 }
